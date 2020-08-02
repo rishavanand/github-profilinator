ca7ab1cb7d16a9ca5286c010ce9c36d984027e6c
Rishav Anand <rishavdipsite@gmail.com>
2020-08-02 12:09:17 +0530

    add type to context data

--- src/context/GlobalContextProvider.tsx
+++ src/context/GlobalContextProvider.tsx
@@ -3,8 +3,6 @@ import React, { useState } from 'react';
 import { SECTION_TYPES } from '../config/global';
 import { DEMO_SECTION_DATA } from '../config/demo';
 
-export const globalContext = React.createContext({});
-
 export interface SectionData {
     type: SECTION_TYPES;
     name: string;
@@ -13,15 +11,16 @@ export interface SectionData {
 }
 
 export interface GlobalContext {
-    activeSectionId: string;
-    changeActiveSection: React.Dispatch<React.SetStateAction<string>>;
-    sections: SectionData[];
-    modifySections: React.Dispatch<React.SetStateAction<SectionData[]>>;
+    activeSectionId?: string;
+    changeActiveSection?: React.Dispatch<React.SetStateAction<string>>;
+    sections?: SectionData[];
+    modifySections?: React.Dispatch<React.SetStateAction<SectionData[]>>;
 }
+
+export const globalContext = React.createContext<GlobalContext>({});
 
 const Provider = (props: { children: React.ReactChildren }) => {
     const [activeSectionId, changeActiveSection] = useState(DEMO_SECTION_DATA[0].id);
-    const [lastActiveSectionId, changeLastActiveSection] = useState();
     const [sections, modifySections] = useState(DEMO_SECTION_DATA);
 
     const globalContextData: GlobalContext = {
