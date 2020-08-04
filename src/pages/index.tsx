a550eef32eb8a38c8e41b0e2ebf9433e619fc86e
Rishav Anand <rishavdipsite@gmail.com>
2020-08-05 02:21:42 +0530

    add loader/spinner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import React, { Component, useContext } from 'react';
-import { Row, Col, Divider, Layout, Menu, Typography, Button } from 'antd';
+import React, { Component, useContext, useState, useEffect } from 'react';
+import { Row, Col, Divider, Layout, Menu, Typography, Button, Spin } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 import styles from '../styles/index.module.scss';
 import Sidebar from '../components/Sidebar';
@@ -11,24 +11,35 @@ const { Content } = Layout;
 
 const IndexPage = () => {
     const context = useContext<GlobalContext>(globalContext);
+    const [initialLoad, setInitialLoad] = useState(false);
+
+    useEffect(() => {
+        setInitialLoad(true);
+    });
+
     return (
-        <Layout>
-            <Sidebar />
-            <Layout className={styles.section}>
-                <Content className={styles.siteLayout}>
-                    <Section
-                        {...context.sections[context.activeSectionIndex]}
-                        sectionIndex={context.activeSectionIndex}
-                        changeColumnCount={context.changeColumnCount}
-                    />
-                </Content>
+        <>
+            <div className={styles.loading} style={{ display: initialLoad ? 'none' : 'flex' }}>
+                <Spin size="large" />
+            </div>
+            <Layout>
+                <Sidebar />
+                <Layout className={styles.section}>
+                    <Content className={styles.siteLayout}>
+                        <Section
+                            {...context.sections[context.activeSectionIndex]}
+                            sectionIndex={context.activeSectionIndex}
+                            changeColumnCount={context.changeColumnCount}
+                        />
+                    </Content>
+                </Layout>
+                <Layout className={styles.section}>
+                    <Content className={[styles.siteLayout, styles.siteLayoutBackground].join(' ')}>
+                        <Preview />
+                    </Content>
+                </Layout>
             </Layout>
-            <Layout className={styles.section}>
-                <Content className={[styles.siteLayout, styles.siteLayoutBackground].join(' ')}>
-                    <Preview />
-                </Content>
-            </Layout>
-        </Layout>
+        </>
     );
 };
 
