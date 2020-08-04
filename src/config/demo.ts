b5d83719810cd40cebd346640db575fd9d8e0301
Rishav Anand <rishavdipsite@gmail.com>
2020-08-04 17:52:01 +0530

    test emoji inputs

--- src/config/demo.ts
+++ src/config/demo.ts
@@ -2,6 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { SECTION_TYPES, FIELD_TYPES } from './global';
 import { FieldProps } from '../components/Field';
 import { SectionProps } from '../components/Section';
+import { TEXT_ALIGNMENT, TEXT_SIZE } from '../components/Field/TextField';
 
 export const DEMO_SECTION_DATA: SectionProps[] = [
     {
@@ -13,18 +14,23 @@ export const DEMO_SECTION_DATA: SectionProps[] = [
                 {
                     id: 'field001',
                     data: {
-                        value: 'Hello, World!',
+                        value: "Hi! 👋 I'm Rishav",
+                    },
+                    options: {
+                        size: TEXT_SIZE.H1,
+                        alignment: TEXT_ALIGNMENT.CENTRE,
                     },
                     type: FIELD_TYPES.TEXT,
                     sectionId: 'section001',
-                    options: {
-                        bold: true,
-                    },
                 },
                 {
                     id: 'field002',
+                    options: {
+                        size: TEXT_SIZE.H3,
+                        alignment: TEXT_ALIGNMENT.CENTRE,
+                    },
                     data: {
-                        value: 'Hello, World!',
+                        value: 'A full-time full-stack freelancer working 💯 remotely since 2013',
                     },
                     type: FIELD_TYPES.TEXT,
                     sectionId: 'section001',
