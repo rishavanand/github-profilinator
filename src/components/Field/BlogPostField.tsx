6d3fbd531a9f6ad0d400569d39ff4e6620c37e5c
Rishav Anand <rishavdipsite@gmail.com>
2020-08-08 21:46:48 +0530

    remove junk from blog post field

--- src/components/Field/BlogPostField.tsx
+++ src/components/Field/BlogPostField.tsx
@@ -1,54 +1,17 @@
 import React, { useContext } from 'react';
 import { Input, Form } from 'antd';
-import { FieldProps } from '.';
-import { globalContext } from '../../context/GlobalContextProvider';
 
-export interface ImageFieldData {
-    url?: string;
-}
-
-export interface BlogPostFieldProps extends FieldProps {
-    id?: string;
-    data?: ImageFieldData;
-}
-
-export const generateBlogPostMarkdown = ({ data }: BlogPostFieldProps) => {
-    if (!data || !data.url) return '';
-    else
-        return `<!-- BLOG-POST-LIST:START --> If things goes well, this section should automatically be replaced by a list of your blog posts after you commit your readme file. \n<!-- BLOG-POST-LIST:END -->`;
+export const generateBlogPostMarkdown = () => {
+    return `<!-- BLOG-POST-LIST:START --> If things goes well, this section should automatically be replaced by a list of your blog posts after you commit your readme file. \n<!-- BLOG-POST-LIST:END -->`;
 };
 
-export const BlogPostField = (
-    blogFieldProps: BlogPostFieldProps &
-        Required<Pick<BlogPostFieldProps, 'sectionIndex' | 'columnIndex' | 'fieldIndex'>>,
-) => {
-    const { modifyField } = useContext(globalContext);
-
-    const localImageFieldProps: typeof blogFieldProps = {
-        options: {},
-        data: {},
-        ...blogFieldProps,
-    };
-
-    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = event.target;
-
-        modifyField({
-            ...localImageFieldProps,
-            data: {
-                ...localImageFieldProps.data,
-                url: value,
-            },
-        });
-    };
-
+export const BlogPostField = () => {
     return (
         <>
-            <Form layout="vertical">
-                <Form.Item label="Blog url">
-                    <Input name="url" value={localImageFieldProps.data.url} onChange={onChange} />
-                </Form.Item>
-            </Form>
+            This is a blog post field. It does not have a URL input box because the URL needs to be added to Github
+            Action. After you add the generated markdown to your README, you have to enable the{' '}
+            <a href="https://github.com/gautamkrishnar/blog-post-workflow">Blog Post Workflow</a> for auto updating
+            README.{' '}
         </>
     );
 };
